da7d096a892db77595f61921ffbac1625f88a2a7
kscheltema <k87scheltema@outlook.com>
2022-09-13 21:07:41 +0200

    notes for tomorrow

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -12,17 +12,18 @@ defmodule Identicon do
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
     hex
+    # use chunk_while
     |> Enum.chunk_every(3)
     |> Enum.map(&mirror_row/1)
-  end
-
-  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def mirror_row(row) do
     [first, second | _tail] = row
     row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
