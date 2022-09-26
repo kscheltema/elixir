4a4b8c9f85ccddfa4909fecad923acb92aa2cb44
kscheltema <k87scheltema@outlook.com>
2022-09-26 15:03:05 +0200

    notes about the variables created within the changeset variable

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -4,6 +4,7 @@ defmodule DiscussWeb.TopicController do
   alias DiscussWeb.Topic
 
   def new(conn, params) do
+    # %Topic is the struct >>>> %{} is the empty params
     changeset = Topic.changeset(%Topic{}, %{})
   end
 end
