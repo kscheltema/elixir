a2fda091d6edbd898a123e90c0ea56889eb23d19
kscheltema <k87scheltema@outlook.com>
2022-09-30 14:03:37 +0200

    error lib/discuss_web/controllers/topic_controller.ex:1: undefined function put_new_layout/2 WIP

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -4,10 +4,16 @@ defmodule DiscussWeb.TopicController do
 
   alias DiscussWeb.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     # %Topic is the struct >>>> %{} is the empty params
     changeset = Topic.changeset(%Topic{}, %{})
 
     render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    Repo.insert(changeset)
   end
 end
