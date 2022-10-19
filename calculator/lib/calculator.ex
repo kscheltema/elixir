92b74c12ade8fa9f1ac33f0e9e0c0eb26c6d496a
kscheltema <k87scheltema@outlook.com>
2022-10-19 10:45:23 +0200

    basic calculator logic

--- calculator/lib/calculator.ex
+++ calculator/lib/calculator.ex
@@ -12,7 +12,33 @@ defmodule Calculator do
       :world
 
   """
-  def hello do
-    :world
+  def start do
+    spawn(fn -> loop(0) end)
+  end
+
+  defp loop(current_value) do
+    new_value =
+      receive do
+        {:view, caller_pid} ->
+          send(caller_pid, {:response, current_value})
+          current_value
+
+        {:add, value} ->
+          current_value + value
+
+        {:sub, value} ->
+          current_value - value
+
+        {:mul, value} ->
+          current_value * value
+
+        {:div, value} ->
+          current_value / value
+
+        _ ->
+          IO.puts("invalid message")
+      end
+
+    loop(new_value)
   end
 end
