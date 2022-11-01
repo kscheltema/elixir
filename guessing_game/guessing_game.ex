31b3fc09f6c19b5cc7f0db8279c8c2ba231b5c24
kscheltema <k87scheltema@outlook.com>
2022-11-01 21:33:21 +0200

    middle number function

--- guessing_game/guessing_game.ex
+++ guessing_game/guessing_game.ex
@@ -1,2 +1,12 @@
 defmodule GuessingGame do
+  # guess between, low, high, middle
+  # tell the user your guess
+  # "yes" -> game over
+  # bigger -> bigger (low & high)
+  # smaller -> smaller (low & high)
+  # anything else -> tell user to enter a valid response
+
+  def mid(low, high) do
+    div(low + high, 2)
+  end
 end
