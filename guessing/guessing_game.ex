58419e1ce8b3e5da74820f193720ced3290deeac
kscheltema <k87scheltema@outlook.com>
2022-11-02 16:24:54 +0200

    instuctions

--- guessing/guessing_game.ex
+++ guessing/guessing_game.ex
@@ -1,5 +1,10 @@
 defmodule GuessingGame do
-
+#guess a number between (low, high) -> guess a number in the middle
+#tell the user your guess
+#"yes" -> game over
+#"bigger" -> bigger(low, high)
+#"smaller" -> smaller(low, high)
+#anything else -> tell the user to enter a valid response
   def mid do
 
   end
