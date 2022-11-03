2e7be8203771fb5e0fecfd40c6d070d90520f6c8
kscheltema <k87scheltema@outlook.com>
2022-11-03 17:15:20 +0200

    smaller, bigger, guess case with user input, and a min max swap guard

--- guessing/guessing_game.ex
+++ guessing/guessing_game.ex
@@ -6,9 +6,19 @@ defmodule GuessingGame do
 #"smaller" -> smaller(low, high)
 #anything else -> tell the user to enter a valid response
 
+  def guess(a, b) when a > b do
+    guess(b, a)
+  end
+
   def guess(low, high) do
-    IO.puts(low)
-    IO.puts(high)
+    answer = IO.gets "Hmm maybe you are thinking of #{mid(low, high)}?\n"
+    case String.trim(answer) do
+      "bigger" -> bigger(low, high)
+      "smaller" -> smaller(low, high)
+      "yes" -> "I knew I could guess your number"
+      _ -> IO.puts ~s{Type "bigger", "smaller" or "yes"}
+      guess(low, high)
+    end
   end
 
   def mid(low, high) do
@@ -16,7 +26,12 @@ defmodule GuessingGame do
   end
 
   def bigger(low,high)do
-    new_low = min(high, mid(low)+1)
+    new_low = min(high, mid(low, high)+1)
     guess(new_low, high)
+  end
+
+  def smaller(low,high)do
+    new_high = max(low, mid(low, high)-1)
+    guess(low, new_high)
   end
 end
