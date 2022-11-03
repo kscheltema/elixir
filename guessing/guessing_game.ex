d31210a9c98bd573da7e67c76a54eeb31b0be6c3
kscheltema <k87scheltema@outlook.com>
2022-11-03 16:08:44 +0200

    investigation on private gremlin like files

--- guessing/guessing_game.ex
+++ guessing/guessing_game.ex
@@ -6,7 +6,17 @@ defmodule GuessingGame do
 #"smaller" -> smaller(low, high)
 #anything else -> tell the user to enter a valid response
 
+  def guess(low, high) do
+    IO.puts(low)
+    IO.puts(high)
+  end
+
   def mid(low, high) do
     div(low + high, 2)
+  end
+
+  def bigger(low,high)do
+    new_low = min(high, mid(low)+1)
+    guess(new_low, high)
   end
 end
