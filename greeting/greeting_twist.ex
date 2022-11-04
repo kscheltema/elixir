c0f736a0b92eade2a8e4a0f6030414caec991092
kscheltema <k87scheltema@outlook.com>
2022-11-04 17:24:01 +0200

    greeting WIP

---
+++ greeting/greeting_twist.ex
@@ -0,0 +1,14 @@
+defmodule GreetingTwist do
+  def greeting do
+   answer = IO.gets().trim "Hello, what is your name?"
+   case String.trim(answer) do
+     "Kobus" -> replyK = IO.gets "Why hello there are you my creator?\n"
+      case String.trim(replyK) do
+        "yes" -> ~s{I am glad I have finally met you.}
+        "no" -> ~s{Kobus must be a popular name then.}
+        _ -> ~s{Sorry I can only handle "no" or "yes"}
+      end
+      _ -> ~s{Hello #{answer}, I am glad to have met you}
+   end
+  end
+end
