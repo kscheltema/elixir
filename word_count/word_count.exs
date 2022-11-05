e06e946c46c9dfd6466320ba35472cb880426386
kscheltema <k87scheltema@outlook.com>
2022-11-05 18:35:13 +0200

    cleaning: unused code & implement the pipe operator

--- word_count/word_count.exs
+++ word_count/word_count.exs
@@ -1,5 +1,5 @@
 filename = IO.gets("File to count the words from: ") |> String.trim()
-words = String.split(File.read!(filename), ~r{(\\n|[^\w'])+})
+words = File.read!(filename)
+  |> String.split(~r{(\\n|[^\w'])+})
   |> Enum.filter(fn x -> x != "" end)
-IO.inspect(words)
 words |> Enum.count() |> IO.puts()
