ae12b96af01ae9b82926de16a8a01800c2da25d5
kscheltema <k87scheltema@outlook.com>
2022-11-05 18:32:39 +0200

    filter of empty List items

--- word_count/word_count.exs
+++ word_count/word_count.exs
@@ -1,4 +1,5 @@
 filename = IO.gets("File to count the words from: ") |> String.trim()
 words = String.split(File.read!(filename), ~r{(\\n|[^\w'])+})
+  |> Enum.filter(fn x -> x != "" end)
 IO.inspect(words)
 words |> Enum.count() |> IO.puts()
