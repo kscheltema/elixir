2113867e2c8342b67b71c46022043c49cfe74d67
kscheltema <k87scheltema@outlook.com>
2022-11-06 19:39:02 +0200

    Challenge 2 WIP

--- word_count/word_count.exs
+++ word_count/word_count.exs
@@ -1,5 +1,14 @@
 filename = IO.gets("File to count the words from: ") |> String.trim()
-words = File.read!(filename)
+option = IO.gets ~s{Would you like to count: "words", "lines" or "characters"? }
+case String.trim(option) do
+  "words" -> words = File.read!(filename)
   |> String.split(~r{(\\n|[^\w'])+})
   |> Enum.filter(fn x -> x != "" end)
-words |> Enum.count() |> IO.puts()
+  words |> Enum.count() |> IO.puts()
+  "characters" -> characters = File.read!(filename)
+  |> String.split(~r{[^\w]+})
+  |> Enum.filter(fn x -> x != "" end)
+  characters |> Enum.count() |> IO.puts()
+  "lines"
+  _ -> ~s{Sorry that is not a valid option}
+end
